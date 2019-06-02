cf385832098568e094b120aa834895906f8d8092
William Fleurant <william@netblazr.com>
2019-06-02 16:31:01 -0400

    espressobin: packages like uapmesh-ac

--- communities/massmesh/meshnode/espressobin/settings.sh
+++ communities/massmesh/meshnode/espressobin/settings.sh
@@ -7,6 +7,6 @@ export VERSION="snapshots"
 export TARGET="mvebu/cortexa53"
 export PROFILE="globalscale_espressobin"
 
-export PACKAGES="${PACKAGES} yggdrasil tcpdump swconfig kmod-ipt-offload kmod-sched-act-vlan luci"
+export PACKAGES="${PACKAGES} -wpad-basic ath10k-firmware-qca988x-ct  batctl-full  cjdns  haveged iperf3 kmod-ath10k-ct  kmod-batman-adv  kmod-ipt-offload kmod-sched-act-vlan  luci  luci-app-cjdns mini_snmpd  nano swconfig tcpdump  wpad-mesh-openssl yggdrasil"
 
 set +x
