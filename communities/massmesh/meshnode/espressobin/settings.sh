2f89982452272d5c777fe1447edc1b009c6501c4
William Fleurant <william@netblazr.com>
2019-06-07 23:23:18 -0400

    espressobin: meta stack

--- communities/massmesh/meshnode/espressobin/settings.sh
+++ communities/massmesh/meshnode/espressobin/settings.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-set -x
-
 export VERSION="snapshots"
 
 export TARGET="mvebu/cortexa53"
 export PROFILE="globalscale_espressobin"
 
-export PACKAGES="${PACKAGES} -wpad-basic ath10k-firmware-qca988x-ct  batctl-full  cjdns  haveged iperf3 kmod-ath10k-ct  kmod-batman-adv  kmod-ipt-offload kmod-sched-act-vlan  luci  luci-app-cjdns mini_snmpd  nano swconfig tcpdump  wpad-mesh-openssl yggdrasil"
+export PACKAGES=" ${PACKAGES} -wpad-basic ath10k-firmware-qca988x-ct"
+export PACKAGES=" ${PACKAGES} batctl-full cjdns haveged iperf3 kmod-ath10k-ct"
+export PACKAGES=" ${PACKAGES} kmod-batman-adv kmod-ipt-offload kmod-sched-act-vlan luci"
+export PACKAGES=" ${PACKAGES} luci-app-cjdns mini_snmpd nano"
+export PACKAGES=" ${PACKAGES} tcpdump wpad-mesh-openssl yggdrasil"
+export PACKAGES=" ${PACKAGES} dnscrypt-proxy dnscrypt-proxy-resolvers luci-app-dnscrypt-proxy"
+export PACKAGES=" ${PACKAGES} luci-theme-material ipset kmod-ipt-ipset"
+export PACKAGES=" ${PACKAGES} tor-fw-helper tor-resolve torsocks tor socat kmod-ipt-nat6"
+export PACKAGES=" ${PACKAGES} node node-npm kmod-usb-net-rndis"
 
-set +x
+
