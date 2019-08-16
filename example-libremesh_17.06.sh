17cbddf3e7da950d4dee44d5869e78ef6ca452dc
Ilario Gelmetti <iochesonome@gmail.com>
2019-08-16 19:25:55 +0200

    make example LibreMesh 17.06 matching the released packages list

--- example-libremesh_17.06.sh
+++ example-libremesh_17.06.sh
@@ -3,7 +3,7 @@ export IB_VERSION="17.01.6"
 export DISTRO="lime"
 export TARGET="ar71xx/generic"
 export PROFILE="tl-wdr3600-v1"
-export PACKAGES="lime-system lime-proto-wan lime-hwd-openwrt-wan lime-debug smonit lime-proto-bmx6 bmx6-auto-gw-mode luci lime-docs lime-docs-minimal luci-mod-admin-full lime-map-agent lime-proto-batadv lime-proto-anygw dnsmasq-lease-share dnsmasq-distributed-hosts lime-webui lime-hwd-ground-routing -dnsmasq"
+export PACKAGES="lime-system lime-proto-wan lime-hwd-openwrt-wan lime-debug smonit lime-proto-bmx6 bmx6-auto-gw-mode luci lime-docs lime-docs-minimal luci-mod-admin-full lime-map-agent lime-proto-batadv lime-proto-anygw dnsmasq-lease-share dnsmasq-distributed-hosts lime-webui lime-hwd-ground-routing -dnsmasq -luci-app-firewall -luci-proto-ppp -luci"
 export REPOS="""src/gz reboot_core http://downloads.openwrt.org/releases/{{ ib_version }}/targets/{{ target }}/packages
 src/gz reboot_base http://downloads.openwrt.org/releases/{{ ib_version }}/packages/{{ pkg_arch }}/base
 src/gz reboot_luci http://downloads.openwrt.org/releases/{{ ib_version }}/packages/{{ pkg_arch }}/luci
