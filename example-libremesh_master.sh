0a32acaab7a6dc60b9b2f27f9a7ac3d4cd1df589
Ilario Gelmetti <iochesonome@gmail.com>
2019-09-20 17:26:03 +0200

    update example libremesh master packages

--- example-libremesh_master.sh
+++ example-libremesh_master.sh
@@ -2,9 +2,8 @@ export VERSION="master"
 export IB_VERSION="18.06.4"
 export DISTRO="lime"
 export TARGET="ar71xx/generic"
-export SUBTARGET="generic"
 export PROFILE="tl-wdr3600-v1"
-export PACKAGES="check-date-http first-boot-wizard hotplug-initd-services lime-app lime-debug lime-hwd-ground-routing lime-hwd-openwrt-wan lime-proto-anygw lime-proto-babeld lime-proto-batadv lime-proto-wan lime-system shared-state shared-state-babeld_hosts shared-state-dnsmasq_hosts shared-state-bat_hosts shared-state-dnsmasq_leases shared-state-nodes_and_links lime-docs lime-docs-minimal libremap-agent -dnsmasq -firewall"
+export PACKAGES="lime-system lime-proto-babeld lime-proto-batadv lime-proto-anygw lime-proto-wan lime-hwd-openwrt-wan shared-state hotplug-initd-services shared-state-babeld_hosts shared-state-bat_hosts shared-state-dnsmasq_hosts shared-state-dnsmasq_leases shared-state-nodes_and_links check-date-http lime-app lime-docs-minimal first-boot-wizard lime-map-agent lime-docs lime-hwd-ground-routing lime-debug luci-lib-libremap-babeld ubus-lime-batman-adv ubus-lime-fbw pirania pirania-app shared-state-pirania shared-state-persist -dnsmasq -firewall -odhcpd-ipv6only -ppp"
 export REPOS="""src/gz v18064_core http://downloads.openwrt.org/releases/{{ ib_version }}/targets/{{ target }}/packages
 src/gz v18064_base http://downloads.openwrt.org/releases/{{ ib_version }}/packages/{{ pkg_arch }}/base
 src/gz v18064_luci http://downloads.openwrt.org/releases/{{ ib_version }}/packages/{{ pkg_arch }}/luci
