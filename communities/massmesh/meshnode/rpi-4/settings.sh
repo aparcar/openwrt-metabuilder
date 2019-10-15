672aa49597b55361d80e4abab09e8258780c4044
Stephen Smith <stephen304@gmail.com>
2019-10-15 18:16:12 -0400

    Add luci to all mesh nodes, add amazon basics gigabit usb ethernet adapter to rpi4

--- communities/massmesh/meshnode/rpi-4/settings.sh
+++ communities/massmesh/meshnode/rpi-4/settings.sh
@@ -4,3 +4,6 @@ export VERSION="snapshots"
 
 export TARGET="brcm2708/bcm2711"
 export PROFILE="rpi-4"
+
+# Amazon basics USB ethernet adapter
+export PACKAGES="${PACKAGES} kmod-usb-net-asix-ax88179"
